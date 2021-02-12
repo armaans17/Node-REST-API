2fc45c1ff6d4667e30dfd1450365fe8b5bc1f5fb
Armaan Singh <armaanmaani@gmail.com>
2021-02-11 21:29:44 -0500

    Mongoose Model and Middleware

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,23 +1,67 @@
 const express = require("express");
+const subscriber = require("../models/subscriber");
 const router = express.Router();
+const Subscriber = require("../models/subscriber");
 
 // Getting all
-router.get("/", (req, res) => {
-  res.send("Home");
+router.get("/", async (req, res) => {
+  try {
+    const subscribers = await Subscriber.find();
+
+    res.status(200).json(subscribers);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 //Getting One
-router.get("/:id", (req, res) => {
-  res.send(req.params.id);
+router.get("/:id", getSubscriber, async (req, res) => {
+  res.json(res.subscriber);
 });
 
 //Creating One
-router.post("/", (req, res) => {});
+router.post("/", async (req, res) => {
+  const subscriber = new Subscriber({
+    name: req.body.name,
+    subscribedToChannel: req.body.subscribedToChannel,
+  });
+
+  try {
+    const newSubscriber = await subscriber.save();
+    res.status(201).json(newSubscriber);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
 //Updating One
-router.patch("/:id", (req, res) => {});
+router.patch("/:id", getSubscriber, async (req, res) => {});
 
 //Deleting One
-router.get("/:id", (req, res) => {});
+router.get("/:id", getSubscriber, async (req, res) => {
+  try {
+    await res.subscriber.remove();
+
+    res.json({ message: "Deleted" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Middleware
+async function getSubscriber(req, res, next) {
+  let subscriber;
+  try {
+    subscriber = await Subscriber.findById(req.params.id);
+    if (!subscriber) {
+      return res.status(404).json({ message: "Cannot find Subscriber" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+
+  res.subscriber = subscriber;
+  next();
+}
 
 module.exports = router;
